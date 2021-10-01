f1cbd40e9ff48cb111acd73c542dc56d61d2cb3f
Jeff <dshostakovich30@gmail.com>
2021-10-01 13:22:45 -0500

    initial commit

---
+++ dogs/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Breed(models.Model):
+
+    TINY = "Tiny"
+    SMALL = "Small"
+    MEDIUM = "Medium"
+    LARGE = "Large"
+
+    SIZE_CHOICES = [(TINY, "Tiny"), (SMALL, "Small"), (MEDIUM, "Meduim"), (LARGE, "Large")]
+
+    name = models.CharField(max_length=50)
+    size = models.CharField(choices=SIZE_CHOICES, default=MEDIUM, max_length=6)
+    friendliness = models.PositiveIntegerField(default=1,
+                                               validators=[MaxValueValidator(5), MinValueValidator(1)])
+    trainability = models.PositiveIntegerField(default=1,
+                                               validators=[MaxValueValidator(5), MinValueValidator(1)])
+    sheddingamount = models.PositiveIntegerField(default=1,
+                                               validators=[MaxValueValidator(5), MinValueValidator(1)])
+    exerciseneeds = models.PositiveIntegerField(default=1,
+                                               validators=[MaxValueValidator(5), MinValueValidator(1)])
+
+
+
+class Dog(models.Model):
+
+    FEMALE = 'F'
+    MALE = 'M'
+
+    GENDER_CHOICES = [(FEMALE, "Female"), (MALE, "Male")]
+
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(25)])
+    breed = models.ForeignKey(Breed, on_delete=models.DO_NOTHING)
+    gender = models.CharField(choices=GENDER_CHOICES, default=FEMALE, max_length=6)
+    color = models.CharField(max_length=60, blank=True)
+    favoritefood = models.CharField(max_length=60, blank=True)
+    favoritetoy = models.CharField(max_length=60, blank=True)
+
+    def __str__(self) -> str:
+        return self.name.title()
---
+++ dogs/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Dog, Breed
+
+class DogSerializer(ModelSerializer):
+    class Meta:
+        model = Dog
+        fields = "__all__"
+
+class BreedSerializer(ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = "__all__"
